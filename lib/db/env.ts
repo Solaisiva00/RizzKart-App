81e7885fce1e26fe5bb8f49903dcf7692ed463de
spooky0047 <solaisiva415@gmail.com>
2024-05-29 19:34:56 +0530

    final project

--- lib/db/env.ts
+++ lib/db/env.ts
@@ -6,7 +6,7 @@ const envSchema = zod.object({
   GOOGLE_CLIENT_ID: zod.string().nonempty(),
   GOOGLE_CLIENT_SECRET: zod.string().nonempty(),
   NEXTAUTH_URL: zod.string().nonempty(),
-  NEXTAUTTH_SECRE: zod.string().nonempty(),
+  NEXTAUTH_SECRET: zod.string().nonempty(),
 });
 
 export const env = envSchema.parse(process.env);
